4e24770280a2fd458becab86df265502cd6c629b
vbnair <vbnair988@gmail.com>
2024-06-17 10:38:57 +0200

    UI update

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,9 +4,18 @@ import { TypeAnimation } from "react-type-animation";
 // Header with a dynamic type animation
 const Header: React.FC = () => {
   return (
-    <header className="bg-neutral-700 text-white p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">Restaurant Finder</h1>
-      <p className=" text-sm font-semibold">
+    <header className=" mx-1 my-5 p-4 flex justify-between items-center">
+      {/* <h1 className="text-xl font-bold">Restaurant Finder</h1> */}
+      <div
+        // href="/"
+        className="text-sm font-semibold bg-neutral-700 rounded-md p-1"
+      >
+        <span className="text-white mr-1">Restaurant</span>
+        <span className=" w-12 h-8 rounded bg-neutral-100 px-1 text-neutral-700 font-bold">
+          Finder
+        </span>
+      </div>
+      <p className=" text-sm font-semibold mx-2 px-1">
         <TypeAnimation
           sequence={[
             "Good food is the foundation of genuine happiness !", // Types 'One'
--- client/src/components/RestaurantList.tsx
+++ client/src/components/RestaurantList.tsx
@@ -1,181 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { useAppDispatch, useAppSelector } from "../redux/hooks";
-// import { fetchRestaurants, resetRestaurants } from "../redux/restaurantsSlice";
-// import RestaurantItem from "./RestaurantItem";
-// import {
-//   APIProvider,
-//   Map,
-//   ControlPosition,
-//   MapControl,
-//   AdvancedMarker,
-//   useAdvancedMarkerRef,
-//   useMap,
-// } from "@vis.gl/react-google-maps";
-// import PlaceAutocomplete from "./PlaceAutocomplete";
-
-// const apiKey = process.env.REACT_APP_GOOGLE_PLACES_API_KEY || "";
-// const mapId = process.env.REACT_APP_MAP_ID || "";
-
-// const RestaurantList: React.FC = () => {
-//   const dispatch = useAppDispatch();
-//   const { restaurants, status, error } = useAppSelector(
-//     (state) => state.restaurants
-//   );
-//   const [location, setLocation] = useState<{ lat: number; lng: number } | null>(
-//     null
-//   );
-//   const [selectedPlace, setSelectedPlace] =
-//     useState<google.maps.places.PlaceResult | null>(null);
-//   const [markerRef, marker] = useAdvancedMarkerRef();
-
-//   useEffect(() => {
-//     if (location) {
-//       dispatch(fetchRestaurants(location));
-//     }
-//   }, [dispatch, location]);
-
-//   useEffect(() => {
-//     if (selectedPlace && selectedPlace.geometry?.location) {
-//       setLocation({
-//         lat: selectedPlace.geometry.location.lat(),
-//         lng: selectedPlace.geometry.location.lng(),
-//       });
-//     }
-//   }, [selectedPlace]);
-
-//   const handleGetLocation = () => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           setLocation({
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude,
-//           });
-//         },
-//         (error) => {
-//           console.error(error.message);
-//         }
-//       );
-//     } else {
-//       console.error("Geolocation is not supported by this browser.");
-//     }
-//   };
-
-//   const handleReset = () => {
-//     setLocation(null);
-//     setSelectedPlace(null);
-//     dispatch(resetRestaurants());
-//   };
-
-//   return (
-//     <div className=" bg-neutral-300">
-//       <div className="m-2 p-2 flex justify-center items-center">
-//         <APIProvider apiKey={apiKey}>
-//           <Map
-//             mapId={mapId}
-//             style={{
-//               width: "100vw",
-//               height: "50vh",
-//               alignItems: "center",
-//               justifyContent: "center",
-//               padding: "5px",
-//               margin: "5px",
-//             }}
-//             defaultCenter={{ lat: 22.54992, lng: 0 }}
-//             defaultZoom={3}
-//             gestureHandling={"greedy"}
-//             disableDefaultUI={true}
-//           >
-//             <AdvancedMarker ref={markerRef} position={null} />
-//             <MapHandler selectedPlace={selectedPlace} marker={marker} />
-//           </Map>
-//           <MapControl position={ControlPosition.TOP}>
-//             <div className="autocomplete-control">
-//               <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
-//             </div>
-//           </MapControl>
-//         </APIProvider>
-//       </div>
-
-//       <div className=" m-5 p-2 flex flex-col items-center rounded-md">
-//         <div className="flex flex-col items-center mb-4">
-//           <h3 className="text-lg font-semibold mb-2">
-//             Find Nearby Restaurants
-//           </h3>
-//           <div className="flex space-x-4">
-//             <button
-//               className=" bg-neutral-700 text-white px-3 py-1 rounded-md hover:bg-neutral-900"
-//               onClick={handleGetLocation}
-//             >
-//               Search My Location
-//             </button>
-//             <button
-//               className="bg-red-400 text-white px-3 py-1 rounded-md hover:bg-red-500"
-//               onClick={handleReset}
-//             >
-//               Reset
-//             </button>
-//           </div>
-//         </div>
-
-//         {status === "loading" && <p>Loading...</p>}
-//         {status === "succeeded" && (
-//           <div className="flex flex-wrap justify-center gap-4">
-//             {restaurants.map((restaurant, index) => (
-//               <RestaurantItem
-//                 key={index}
-//                 name={restaurant.name}
-//                 vicinity={restaurant.vicinity}
-//                 rating={restaurant.rating}
-//                 userRatingsTotal={restaurant.user_ratings_total}
-//                 distance={restaurant.distance}
-//                 photoUrl={restaurant.photoUrl || undefined}
-//                 placeId={restaurant.place_id} // Pass the place ID
-//               />
-//             ))}
-//           </div>
-//         )}
-//         {status === "failed" && <p>{error}</p>}
-//       </div>
-//     </div>
-//   );
-// };
-
-// interface MapHandlerProps {
-//   selectedPlace: google.maps.places.PlaceResult | null;
-//   marker: google.maps.marker.AdvancedMarkerElement | null;
-// }
-
-// const MapHandler: React.FC<MapHandlerProps> = ({ selectedPlace, marker }) => {
-//   const map = useMap();
-
-//   useEffect(() => {
-//     if (selectedPlace && selectedPlace.geometry?.location && marker && map) {
-//       const location = {
-//         lat: selectedPlace.geometry.location.lat(),
-//         lng: selectedPlace.geometry.location.lng(),
-//       };
-
-//       marker.position = new google.maps.LatLng(location.lat, location.lng);
-
-//       if (selectedPlace.geometry.viewport) {
-//         map.fitBounds(selectedPlace.geometry.viewport);
-//       } else {
-//         map.setCenter(location);
-//         map.setZoom(15);
-//       }
-//     } else if (marker && map) {
-//       marker.position = null;
-//       map.setCenter({ lat: 22.54992, lng: 0 });
-//       map.setZoom(3);
-//     }
-//   }, [selectedPlace, marker, map]);
-
-//   return null;
-// };
-
-// export default RestaurantList;
-
 import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { fetchRestaurants, resetRestaurants } from "../redux/restaurantsSlice";
@@ -252,90 +74,104 @@ const RestaurantList: React.FC = () => {
   };
 
   return (
-    <div className=" bg-neutral-300">
-      <div className="m-2 p-2 flex justify-center items-center">
-        <APIProvider apiKey={apiKey}>
-          <Map
-            mapId={mapId}
-            style={{
-              width: "100vw",
-              height: "50vh",
-              alignItems: "center",
-              justifyContent: "center",
-              padding: "5px",
-              margin: "5px",
-            }}
-            defaultCenter={{ lat: 22.54992, lng: 0 }}
-            defaultZoom={3}
-            gestureHandling={"greedy"}
-            disableDefaultUI={true}
-          >
-            <AdvancedMarker ref={markerRef} position={null} />
-            <MapHandler selectedPlace={selectedPlace} marker={marker} />
-          </Map>
-          <MapControl position={ControlPosition.TOP}>
-            <div className="autocomplete-control">
-              <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
+    <div className="">
+      <div>
+        <div className="hero min-h-screen bg-gradient-to-r from-cyan-50 to-sky-500">
+          <div className="hero-overlay bg-opacity-60"></div>
+          {/* <div className="hero-content text-center text-neutral-content"> */}
+          <div className="hero-content flex flex-col items-center justify-start text-neutral-content h-full w-full">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-3xl font-bold">
+                Find Nearby Restaurants
+              </h1>
+              <p className="mb-5">
+                Easily find nearby restaurants, Discover new dining options in
+                their vicinity with their distance from the user's location,
+                ratings, and a link to view them on Google Maps..
+              </p>
+              {/* <button className="btn btn-primary">Get Started</button> */}
+              <button
+                className=" bg-neutral-700 text-white px-3 py-1 rounded-md hover:bg-neutral-900 mx-2"
+                onClick={handleGetLocation}
+              >
+                Search My Location
+              </button>
+              <button
+                className="bg-red-400 text-white px-3 py-1 rounded-md hover:bg-red-500 mx-2"
+                onClick={handleReset}
+              >
+                Reset
+              </button>
             </div>
-          </MapControl>
-        </APIProvider>
-      </div>
 
-      <div className=" m-5 p-2 flex flex-col items-center rounded-md">
-        <div className="flex flex-col items-center mb-4">
-          <h3 className="text-lg font-semibold mb-2">
-            Find Nearby Restaurants
-          </h3>
-          <div className="flex space-x-4">
-            <button
-              className=" bg-neutral-700 text-white px-3 py-1 rounded-md hover:bg-neutral-900"
-              onClick={handleGetLocation}
-            >
-              Search My Location
-            </button>
-            <button
-              className="bg-red-400 text-white px-3 py-1 rounded-md hover:bg-red-500"
-              onClick={handleReset}
-            >
-              Reset
-            </button>
+            {status === "loading" && <p>Loading...</p>}
+            {status === "succeeded" && restaurants.length > 0 && (
+              <div className="flex flex-wrap justify-center gap-4">
+                {restaurants.map((restaurant, index) => (
+                  <RestaurantItem
+                    key={index}
+                    name={restaurant.name}
+                    vicinity={restaurant.vicinity}
+                    rating={restaurant.rating}
+                    userRatingsTotal={restaurant.user_ratings_total}
+                    distance={restaurant.distance}
+                    photoUrl={restaurant.photoUrl || undefined}
+                    placeId={restaurant.place_id} // Pass the place ID
+                  />
+                ))}
+              </div>
+            )}
+            {status === "succeeded" && restaurants.length === 0 && (
+              <div className="flex flex-col items-center mt-4">
+                <p>No restaurants found</p>
+              </div>
+            )}
+            {status === "failed" && <p>{error}</p>}
+            {/* Show the static image when no restaurants are displayed */}
+            {showStaticImage && (
+              <div className="mt-4 w-full flex justify-center items-center">
+                <img
+                  className=" rounded-md"
+                  src={defaultImage}
+                  alt="Default"
+                  style={{
+                    width: "100%",
+                    height: "50vh",
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+            )}
           </div>
+          <div className=" bg-white w-full"></div>
         </div>
-
-        {status === "loading" && <p>Loading...</p>}
-        {status === "succeeded" && restaurants.length > 0 && (
-          <div className="flex flex-wrap justify-center gap-4">
-            {restaurants.map((restaurant, index) => (
-              <RestaurantItem
-                key={index}
-                name={restaurant.name}
-                vicinity={restaurant.vicinity}
-                rating={restaurant.rating}
-                userRatingsTotal={restaurant.user_ratings_total}
-                distance={restaurant.distance}
-                photoUrl={restaurant.photoUrl || undefined}
-                placeId={restaurant.place_id} // Pass the place ID
-              />
-            ))}
-          </div>
-        )}
-        {status === "succeeded" && restaurants.length === 0 && (
-          <div className="flex flex-col items-center mt-4">
-            <p>No restaurants found</p>
-          </div>
-        )}
-        {status === "failed" && <p>{error}</p>}
-
-        {/* Show the static image when no restaurants are displayed */}
-        {showStaticImage && (
-          <div className="mt-4 w-full flex justify-center items-center">
-            <img
-              src={defaultImage}
-              alt="Default"
-              style={{ width: "100%", height: "50vh", objectFit: "cover" }}
-            />
-          </div>
-        )}
+        <div className="flex justify-center items-center">
+          <APIProvider apiKey={apiKey}>
+            <Map
+              mapId={mapId}
+              style={{
+                width: "100vw",
+                height: "50vh",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: "5px",
+                margin: "5px",
+              }}
+              defaultCenter={{ lat: 22.54992, lng: 0 }}
+              defaultZoom={3}
+              gestureHandling={"greedy"}
+              disableDefaultUI={true}
+            >
+              <AdvancedMarker ref={markerRef} position={null} />
+              <MapHandler selectedPlace={selectedPlace} marker={marker} />
+            </Map>
+            <MapControl position={ControlPosition.TOP}>
+              <div className="autocomplete-control">
+                <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
+              </div>
+            </MapControl>
+          </APIProvider>
+        </div>
       </div>
     </div>
   );
